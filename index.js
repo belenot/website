3559dd1d60bac0b449059cd86514704a7fd87e04
belenot <kholhunov0@gmail.com>
2021-01-20 20:41:56 +0300

    Init commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const staticContentPath = path.join(__dirname, 'static')
+const port = 8080
+
+const app = express()
+
+app.use(express.static(staticContentPath))
+
+app.get("/health", (req, res) => {
+    res.status(200).end()
+})
+
+app.listen(port, () => {
+    console.log(`Started server on port ${port}`)
+})
